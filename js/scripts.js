fee61758cfa9b2eab488a2001c67e2c7f79c112b
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-01-20 08:38:38 -0500

    Add: Add h2 heading
     Changes to be committed:
    	modified:   css/styles.css
    	modified:   index.html
    	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@ var markers = [];
 
 //make AJAX call to foursquare API
 var result = function(){
-  var AllPlaces = [];
+  var bounds = new google.maps.LatLngBounds();
+  var infowindowList = new google.maps.InfoWindow();
   var myAddress = '383 Springfield Ave, Newark NJ 07103';
   var foursquareUrl2 = 'https://api.foursquare.com/v2/venues/search?limit=80&near=' + myAddress + '&client_id=RMJLP0XC1CPMPOE4IINGN4RSBFUVTP10D1N0OO0RLBCCNPFK&client_secret=2OAXA5CSE43HQRNUYEEFV2FYR3SLB3CHVN0FCLXEGIUVCUUP&v=20170801';
 
@@ -14,21 +15,30 @@ var result = function(){
     for (var i = 0; i < resultParsed.length; i++) {
       var resp = resultParsed[i]; var llresp = resp.location;
       if (resp.contact.formattedPhone && resp.name.length <= 16) {
-        AllPlaces.push({
-          name: resp.name,
-          Address: llresp.formattedAddress,
-          Phone: resp.contact.formattedPhone,
-          stats: resp.stats,
-          latlng:[{lat: llresp.lat, lng: llresp.lng}]});
-          if(AllPlaces.length == 8){
-            return false;
-          }
+        var maklocation = {lat: llresp.lat, lng: llresp.lng};
+        var makaddress = llresp.formattedAddress;
+        var maktitle = resp.name;
+        var makphone = resp.contact.formattedPhone;
+        var makstats = "checkinsCount : " +  resp.stats.checkinsCount + ", " + " tipCount : " + resp.stats.tipCount + ", " + " usersCount : " + resp.stats.usersCount;
+        var marker = new google.maps.Marker({
+          position: maklocation,
+          map: map,
+          name: maktitle,
+          animation: google.maps.Animation.DROP,
+          cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makphone + '<br>' + makstats + '<br>' + "source: Foursquare API",
+          id: i
+        });
+        if(markers.length == 8){return false;}
+        markers.push(marker);
+        bounds.extend(marker.position);
+        showInfoWindow(marker, infowindowList);
       }
+      map.fitBounds(bounds);
     }
   }).fail(function(xhr, errorType, exception) {
     alert( "Failed to connect to Foursquare API : " + xhr.textstatus + " " + errorType + "\n " + exception);
   });
-  return AllPlaces;
+  return markers;
 }
 
 
@@ -45,8 +55,6 @@ function showInfoWindow(marker, infowindowList) {
   marker.addListener('click', function(){
     infowindowList.setContent(marker.cursor);
     infowindowList.open(map, marker);
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-    setTimeout(function () {marker.setAnimation(null);}, 1400);
   });
   marker.addListener('mouseout', function(){
     infowindowList.close(map, marker);
