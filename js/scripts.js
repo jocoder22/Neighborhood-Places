e9eb5c738566bae0af30ed83e9695c9036b38eec
Okechukwu J. Okigbo <okigbookey@gmail.com>
2018-01-20 22:52:02 -0500

    refactor: Add filter handler
     Changes to be committed:
    	modified:   js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 var map;
 var markers = [];
 
+
 //make AJAX call to foursquare API
 var result = function(){
   var bounds = new google.maps.LatLngBounds();
@@ -19,14 +20,15 @@ var result = function(){
         var makaddress = llresp.formattedAddress;
         var maktitle = resp.name;
         var makphone = resp.contact.formattedPhone;
-        var makstats = "checkinsCount : " +  resp.stats.checkinsCount + ", " + " tipCount : " + resp.stats.tipCount + ", " + " usersCount : " + resp.stats.usersCount;
+        var makstats = "checkinsCount :  " +  resp.stats.checkinsCount + ",  "  + "  usersCount :  " + resp.stats.usersCount;
         var marker = new google.maps.Marker({
           position: maklocation,
           map: map,
           name: maktitle,
           animation: google.maps.Animation.DROP,
           cursor: '<h4>' + maktitle + '</h4>' + makaddress + '<br>' + makphone + '<br>' + makstats + '<br>' + "source: Foursquare API",
-          id: i
+          id: i,
+          location: [maklocation]
         });
         if(markers.length == 8){return false;}
         markers.push(marker);
@@ -34,6 +36,9 @@ var result = function(){
         showInfoWindow(marker, infowindowList);
       }
       map.fitBounds(bounds);
+      google.maps.event.addDomListener(window, 'resize', function() {
+        map.fitBounds(bounds);
+      });
     }
   }).fail(function(xhr, errorType, exception) {
     alert( "Failed to connect to Foursquare API : " + xhr.textstatus + " " + errorType + "\n " + exception);
@@ -41,50 +46,101 @@ var result = function(){
   return markers;
 }
 
+console.log(markers);
+console.log(markers[3]);
+
+// function initMap() {
+//   result();
+//   map = new google.maps.Map(document.getElementById('mm'),{
+//     // center: markers[3].location[0],
+//     zoom: 12
+//   });
+// }
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('mm'))
-  result();
+    mapOptions = {
+      zoom: 10,
+      center: new google.maps.LatLng(40.733553, -74.196379)
+    }
+    mapDocument = document.getElementById('mm')
+    map = new google.maps.Map(mapDocument, mapOptions);
+    result()
 }
 
 function MapError() {
   alert("Google Map error.");
 }
 
+
+
 function showInfoWindow(marker, infowindowList) {
   marker.addListener('click', function(){
     infowindowList.setContent(marker.cursor);
     infowindowList.open(map, marker);
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout(function () {marker.setAnimation(null);}, 700);
   });
   marker.addListener('mouseout', function(){
     infowindowList.close(map, marker);
   });
 }
 
+console.log(markers);
+
+
 var ViewModel = function () {
   var self = this;
   self.query1 = ko.observable('');
-  self.allMarker = ko.observableArray(markers);
+  self.allMarker = ko.observableArray([]);
   self.initialXClass = ko.observable(false);
   self.toggleXClass = function () {
     this.initialXClass(!this.initialXClass());
   };
 
-  self.bounceMarker = function (marker) {
+
+  self.bounceMarker = function (marker, infowindowList) {
     marker.setAnimation(google.maps.Animation.BOUNCE);
     setTimeout(function () {marker.setAnimation(null);}, 1400);
   }
 
+  setTimeout(function() {
+    markers.forEach(function (item) {
+      self.allMarker.push(item);
+    })
+  }, 600);
+
+
+  self.visibleMarkers = function(){
+		self.allMarker().forEach(function(marker){
+			marker.setVisible(true);
+		});
+	};
+
+
   self.filterPlace = ko.computed(function () {
     var myFilter = self.query1().toLowerCase();
     if (!myFilter) {
+      self.visibleMarkers();
       return self.allMarker();
     } else {
-      return ko.utils.arrayFilter(self.allMarker(), function (item) {
-        return item.name.toLowerCase().indexOf(myFilter) >=0;
+       return ko.utils.arrayFilter(self.allMarker(), function(item) {
+        if (item.name.toLowerCase().indexOf(myFilter) >= 0) {
+          item.setVisible(true);
+          return true;
+        } else {
+          item.setVisible(false);
+          return false;
+        };
       });
     }
   });
-};
+
+
+
+
+}
+
+
+
 
 ko.applyBindings(new ViewModel());
